5f8d1a4fee37ffb821ad88e26e792c10bacaf5cf
Vibhavari Bellutagi <vibhavari.bellutagi@gmail.com>
2024-11-25 22:24:59 +0100

    add build

---
+++ build/assets/js/36994c47.4515e2f1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvibhavari_bellutagi=self.webpackChunkvibhavari_bellutagi||[]).push([[858],{5516:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
---
+++ build/assets/js/c15d9823.95c0a778.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkvibhavari_bellutagi=self.webpackChunkvibhavari_bellutagi||[]).push([[146],{9328:a=>{a.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
